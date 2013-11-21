4daf9037b7301b0bfd7305d2fdf67ae15b717509
Javier Domingo Cansino <javierdo1@gmail.com>
2013-11-21 16:18:25 +0100

    ej1: finalizado el ejercicio a duras penas

--- ejercicio_1/circ_arit.vhd
+++ ejercicio_1/circ_arit.vhd
@@ -1,15 +1,15 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
-library WORK;
+library work;
 
 
 entity circ_arit is
 	generic(
 		width: natural := 32);
-   port (
+  port (
    	clk, reset, en: in std_logic;
 		a, b, c, d: in std_logic_vector(width-1 downto 0);
 		aritout: out std_logic_vector(width-1 downto 0));
@@ -21,30 +21,30 @@ signal s1, s2, s3: std_logic_vector(width-1 downto 0);
 begin
 abcdreg: process (clk)
 begin
-   if clk'event and clk='1' then
-		if reset='1' then
-			areg <= (others => '0');
-			breg <= (others => '0');
-			creg <= (others => '0');
-			dreg <= (others => '0');
-   	elsif (en='1') then 
-			areg <= a;
-			breg <= b;
-			creg <= c;
-			dreg <= d;
-   	end if;
-   end if;
+  if clk'event and clk='1' then
+    if reset='1' then
+      areg <= (others => '0');
+      breg <= (others => '0');
+      creg <= (others => '0');
+      dreg <= (others => '0');
+    elsif (en='1') then 
+      areg <= a;
+      breg <= b;
+      creg <= c;
+      dreg <= d;
+    end if;
+  end if;
 end process;
-s1 <= areg + breg;
-s2 <= creg + dreg;
-s3 <= s1 + s2;
+
 aritoutreg: process (clk)
 begin
    if clk'event and clk='1' then
+      s1 <= areg + breg;
+      s2 <= creg + dreg;
 		if reset='1' then
 			aritout <= (others => '0');
    	elsif (en='1') then 
-			aritout <= s3;
+			aritout <= s1+s2;
    	end if;
    end if;
 end process;

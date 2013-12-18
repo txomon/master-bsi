a9d69154b31225bf766208e41454a70e3ce6cd85
Javier Domingo Cansino <javierdo1@gmail.com>
2013-12-18 16:41:44 +0100

    ejercicio_5: C program correction of entries. With non-volatile peripherial addresses, it will optimize code too much

--- ejercicio_5/program/main.c
+++ ejercicio_5/program/main.c
@@ -1,20 +1,19 @@
 #include "xparameters.h"
 #include "stdio.h"
 
-#define WAIT_TIME 1000
+#define WAIT_TIME 1000000
 
 int main ()
 {
-	unsigned int *leds = (unsigned int *) XPAR_LEDS_SWITCH_8_4_BASEADDR;
+	unsigned int *leds = (unsigned int *) (XPAR_LEDS_SWITCH_8_4_BASEADDR + 0x00);
 	unsigned int *switches = (unsigned int *) (XPAR_LEDS_SWITCH_8_4_BASEADDR + 0x08);
-	int waiting;
-	unsigned int temp;
+	volatile int waiting;
+	volatile unsigned int temp;
 	while(1)
 	{
-		temp = *switches;
-		*leds = temp;
-		for (waiting = 0 ; waiting < WAIT_TIME ; waiting++){
-			asm("");
+		*leds = *switches + 1;
+		for (waiting = 0 ; waiting < WAIT_TIME;){
+			waiting++;
 		}
 	}
 	
